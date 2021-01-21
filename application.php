3ff3e63cd302038d5933b23753c06db5935c458a
rdhimanaiopsgroup <rdhiman@aiopsgroup.com>
2021-01-21 16:50:44 +0200

    Folder Structure Changes

--- application.php
+++ application.php
@@ -3,7 +3,7 @@
 *   Result will output the soonest time at which each of the commands will fire and whether it is today or  tomorrow. 
 *   @author Rohit Dhiman rdhiman@aiopsgroup.com
 */
-require 'Nextcrontime.php';
+require 'src/Nextcrontime.php';
 use AIopsGroup\CronTime\Nextcrontime as CalculateTimeClass;
 If(isset($argv) && !empty($argv[1]) && !empty($argv[2] )){
     $execute = new CalculateTimeClass();
