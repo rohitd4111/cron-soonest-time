2843b804d43fd9d264fa683c50f628a19aed10cf
rdhimanaiopsgroup <rdhiman@aiopsgroup.com>
2021-01-22 13:40:09 +0200

    Added PSR 14 coding standards

--- application.php
+++ application.php
@@ -1,15 +1,14 @@
 <?php
 /*  This script takes time as single argument.
-*   Result will output the soonest time at which each of the commands will fire and whether it is today or  tomorrow. 
+*   Result will output the soonest time at which each of the commands will fire and whether it is today or  tomorrow.
 *   @author Rohit Dhiman rdhiman@aiopsgroup.com
 */
 require 'src/Nextcrontime.php';
 use AIopsGroup\CronTime\Nextcrontime as CalculateTimeClass;
-If(isset($argv) && !empty($argv[1]) && !empty($argv[2] )){
+if (isset($argv) && !empty($argv[1]) && !empty($argv[2])) {
     $execute = new CalculateTimeClass();
     echo $execute->calculatenextCron();
-}else{
+} else {
     echo "Invalid Format. Example - php Index.php 16:10 FILENAME";
     return;
 }
-?>
